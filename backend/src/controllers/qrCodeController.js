6307f56ca971fb7452b860c821252a6ea8ec4728
Tushar Kumar <167692314+curioustushaar@users.noreply.github.com>
2026-03-23 07:00:33 +0000

    fix: remove unused uuid import causing ESM/CommonJS conflict in Vercel

--- backend/src/controllers/qrCodeController.js
+++ backend/src/controllers/qrCodeController.js
@@ -1,5 +1,4 @@
 const QRCode = require('qrcode');
-const { v4: uuidv4 } = require('uuid');
 const Room = require('../models/Room');
 const Booking = require('../models/Booking');
 const QRCodeModel = require('../models/QrCode');
